4a108a6d7062339bfd5288e882403e5dfdc6f85e
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2025-04-28 21:10:08 +0100

    cors again

--- src/main/java/gp2/SCRM/User/UserService/config/SecurityConfiguration.java
+++ src/main/java/gp2/SCRM/User/UserService/config/SecurityConfiguration.java
@@ -51,7 +51,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
 
